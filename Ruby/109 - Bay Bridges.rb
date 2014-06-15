cc723394a09cb41af4badd76d14de9fe782fbe94
Rob Freiburger <rob@robfreiburger.com>
2014-06-15 16:21:21 -0400

    Implements Bridge class.

--- Ruby/109 - Bay Bridges.rb
+++ Ruby/109 - Bay Bridges.rb
@@ -1,3 +1,31 @@
+# Solution influenced by https://gist.github.com/anymd/8450736
+# Make basic Bridge class for easier data management
+class Bridge
+  def initialize(pointA, pointB, name)
+    @pointA_X, @pointA_Y = pointA[0].to_f, pointA[1].to_f
+    @pointB_X, @pointB_Y = pointB[0].to_f, pointB[1].to_f
+    @name = name
+  end
+  
+  def slope
+    (@pointB_Y - @pointA_Y) / (@pointB_X - @pointA_X)
+  end
+  
+  def horizontal_intercept
+    @pointA_Y - self.slope * @pointA_X
+  end
+  
+  def name
+    "#{@name}"
+  end
+  
+  # Example output: "1: ([37.788353, -122.387695], [37.829853, -122.294312])"
+  def to_s
+    "#{@name}: ([#{@pointA_X}, #{@pointA_Y}], [#{@pointB_X}, #{@pointB_Y}])"
+  end
+end
+
+# Input data
 test_cases = []
 open(ARGV[0]) do |f|
   while (line = f.gets)
@@ -8,4 +36,3 @@ open(ARGV[0]) do |f|
     test_cases << [points[0..1], points[2..3]]
   end
 end
-
